ffc55fcba210ae87ad24a05335b697e06d9e8064
DiegoOno <diego.ono1505@gmail.com>
2018-09-21 10:42:46 -0300

    Initialized some nodes

--- main.py
+++ main.py
@@ -6,14 +6,51 @@ def main():
     #Something like:
     BayesianTree = [] #This should be a class too ?
 
-    node = Node('PKC')
-    node.createState('LOW', 0.42313152)
-    node.createState('AVG', 0.48163920)
-    node.createState('HIGH', 0.09522928)
+    node0 = Node('PKC')
+    node0.createState('LOW', 0.42313152)
+    node0.createState('AVG', 0.48163920)
+    node0.createState('HIGH', 0.09522928)
 
-    BayesianTree.append(node)
+    BayesianTree.append(node0)
 
-    print(node.getStateProb('LOW'))
+    node1 = Node('Plcg')
+    node1.createState('LOW', 0.81213356)
+    node1.createState('AVG', 0.08337962)
+    node1.createState('HIGH', 0.10448682)
+    
+    BayesianTree.append(node1)
+    
+    node2 = Node('PKA')
+    node2Probabilities = {'LOW': [0.3864255, 0.3794243, 0.2341501], 'AVG': [0.06039638, 0.92264651, 0.01695712], 'HIGH': [0.01577014, 0.95873839, 0.02549147]}
+    node2.setProbabilities(node2Probabilities)
+    BayesianTree.append(node2)
+
+    node3 = Node('PIP3')
+    BayesianTree.append(node3)
+
+    node4 = Node('Raf')
+    BayesianTree.append(node4)
+
+    node5 = Node('Jnk')
+    BayesianTree.append(node5)
+
+    node6 = Node('P38')
+    BayesianTree.append(node6)
+
+    node7 = Node('PIP2')
+    BayesianTree.append(node7)
+
+    node8 = Node('Mek')
+    BayesianTree.append(node8)
+    
+    node9 = Node('Erk')
+    BayesianTree.append(node9)
+    
+    node10 = Node('Akt')
+    BayesianTree.append(node10)
+    #print(node.getStateProb('LOW'))
+
+    #print(node2.probabilities)
 
 if (__name__ == '__main__'):
     main()
--- node.py
+++ node.py
@@ -5,6 +5,7 @@ class Node:
 
     def __init__(self, name):
         self.name = name
+        self.probabilities = []
 
     def createState(self, stateName, probability):
         if (stateName.upper() in POSSIBLE_STATES):
@@ -12,6 +13,9 @@ class Node:
         else:
             raise ValueError("Invalid state: " + stateName +
                     ". Valid states are: " + str(POSSIBLE_STATES))
+
+    def setProbabilities(self, dict):
+        self.probabilities = dict
 
     def getStateProb(self, stateName):
         return self.stateProb[stateName]
